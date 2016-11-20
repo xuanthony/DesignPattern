05f7ab9708fa1107722657fec3bdfa41d07e5194
xuanthony <xuanthony@sina.com>
2016-11-20 20:19:34 +0800

    复习23种设计模式

---
+++ src/main/java/com/xuxb/produser_consumer/ProductStorage.java
@@ -0,0 +1,11 @@
+package com.xuxb.produser_consumer;
+
+/**
+ * @（#）:ProductStorage
+ * @description:
+ *              库存
+ * @author: xuxb 16/8/2
+ * @version: Version 1.0
+ */
+public class ProductStorage {
+}
